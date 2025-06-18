19b973ea79cb632261ce753233487ade971126a8
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-18 19:33:39 +0300

    Adds db.json index.js sample_form.html

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(usersName, usersEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: usersName,
+      email: usersEmail
+    })
+  })
+  .then(response => response.json()) 
+  .then(data => {
+    
+    const newElement = document.createElement('p');
+    newElement.textContent = `User ID: ${data.id}`;
+    document.body.appendChild(newElement);
+  })
+  .catch(error => {
+    const errorElement = document.createElement('p');
+    errorElement.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorElement);
+  });
+}
+    
